0c3bb9fa82f7826ea0832092a0da16c687eb14fe
Christie <cholvie@gmail.com>
2019-04-06 16:19:30 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+var landing = require('../controllers/landing');
+
+/* GET home page. */
+router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads);
+
+module.exports = router;
